2bc45c933f136b57100eb9c6814da0edcfd7d257
ecorbalan <ezequiel.corbalan@experienciafutbol.com>
2020-08-09 12:03:55 -0300

    .

--- config/config.js
+++ config/config.js
@@ -1,22 +1,7 @@
 module.exports = {
-  HOST: "aurora-integrations-test.cweau7nxx7ej.us-east-1.rds.amazonaws.com",
-  USER: "icomm",
-  PASSWORD: "icommpsw",
-  DB: "EcommerceIntegration",
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DB: "lol",
   dialect: "mysql",
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  key : "%8e87kvH:A4Sjq*cvq;gT(N\"HGCS/]",
-  aws : {
-    accessKey : "AKIARVOXYNW4O5KFDHXC" ,
-    secretKey : "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"
-  },
-  dynamo : {
-    table : "TestClientIntegration"
-  }
 };
